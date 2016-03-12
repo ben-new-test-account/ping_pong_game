a046eff58dc55e4e7a5d0ab5f1853d389f61a176
Ben Zittlau <ben@zittlau.ca>
2016-03-12 11:22:43 -0700

    Add the pong class.

--- ping_pong.py
+++ ping_pong.py
@@ -9,4 +9,11 @@ import time
 # Write Pong After Here
 ######################
 
+class Pong:
+    @staticmethod
+    def bounce():
+        print "Pong"
+        time.sleep(1)
+        Ping.bounce()
+
 Ping.bounce()
