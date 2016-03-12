28bf4768d58c5fa3f438e207be265edc705865c6
Ben Zittlau <ben@zittlau.ca>
2016-03-12 11:20:04 -0700

    Add initial placeholder file.

---
+++ ping_pong.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import time
+
+######################
+# Write Ping After Here
+######################
+
+######################
+# Write Pong After Here
+######################
+
+Ping.bounce()
