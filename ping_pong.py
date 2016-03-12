db804c9caa8df4843a21e7aa640e649598ac7a98
Alexander Wong <admin@alexander-wong.com>
2016-03-12 11:21:40 -0700

    Added the Ping class with the bounce method
    - Also added a git ignore.

--- ping_pong.py
+++ ping_pong.py
@@ -5,6 +5,13 @@ import time
 # Write Ping After Here
 ######################
 
+class Ping:
+    @staticmethod
+    def bounce():
+        print "Ping"
+        time.sleep(1)
+        Pong.bounce()
+
 ######################
 # Write Pong After Here
 ######################
